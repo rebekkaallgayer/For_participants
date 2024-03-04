6aa8f6b3e8ff4f74ee05b356ef44283fb5368a5e
rebekkaallgayer <rebekka.allgayer3@abdn.ac.uk>
2024-03-04 05:06:35 +0000

    create repo

---
+++ Data visualisation.R
@@ -0,0 +1,153 @@
+##Data exploration and Visualisation 
+#Material written by Dr. Alex Douglas at Aberdeen University
+#you can find the instructions here: https://alexd106.github.io/QUADstatR/exercise_4.html for more details and background
+
+#set your working directory
+setwd() #this is the path to where you have stored the data
+#for example, if you saved your data in Documents/workshop, your path would be "C:\Documents\workshop"
+
+#this first section is covering basic plots in base R
+
+
+#Import the ‘squid1.txt’ file into R using the read.table() function and assign it to a variable named squid. 
+squid <- read.table("all_data/data/squid1.txt", header=T)
+
+#These data were originally collected as part of a study published in Aquatic Living Resources1 in 2005. 
+#The aim of the study was to investigate the seasonal patterns of investment in somatic and reproductive tissues 
+#in the long finned squid Loligo forbesi caught in Scottish waters. 
+#Squid were caught monthly from December 1989 - July 1991 (month and year variables). 
+#After capture, each squid was given a unique specimen code, weighed (weight) and the sex determined (sex - only female squid are included here). 
+#The size of individuals was also measured as the dorsal mantle length (DML) and the mantle weight measured without internal organs (eviscerate.weight). 
+#The gonads were weighed (ovary.weight) along with the accessory reproductive organ (the nidamental gland, nid.weight, nid.length). 
+#Each individual was also assigned a categorical measure of maturity (maturity.stage, ranging from 1 to 5 with 1 = immature, 5 = mature). 
+#The digestive gland weight (dig.weight) was also recorded to assess nutritional status of the individual
+
+#Use the str() function to display the structure of the dataset and the summary() function to summarise the dataset. 
+
+str(squid)
+
+summary(squid)
+#How many observations are in this dataset? How many variables? 
+
+
+#The year, month and maturity.stage variables were coded as integers in the original dataset. 
+#Here we would like to code them as factors. 
+#Create a new variable for each of these variables in the squid dataframe and recode them as factors. 
+
+#factors are categories. these might be treatments, years, sites, etc
+#imagine that you want to compare trends across years, sites, etc. this is easier to do with factors
+squid$Year_f <- as.factor(squid$year)
+squid$month_f <- as.factor(squid$month)
+squid$maturity.stage_f <- as.factor(squid$maturity.stage)
+
+#Use the str() function again to check the coding of these new variables.
+str(squid)
+
+
+############DOTCHARTS ############
+#The humble cleveland dotplot is a great way of identifying if you have potential outliers in continuous variables. 
+#Create dotplots (using the dotchart() function) for the following variables; DML, weight, nid.length and ovary.weight. 
+
+dotchart(squid$DML)
+dotchart(squid$weight)
+dotchart(squid$nid.length)
+dotchart(squid$ovary.weight)
+#Do these variables contain any unusually large or small observations? 
+
+#it looks like nid.length does! this turned out to be a recording error, and we can fix it
+which(squid$nid.length > 400) #this gives the row of the value that is the outlier
+squid$nid.length[11]
+#then we can go in and change it
+squid$nid.length[11] <- 43.2
+#see if it worked
+dotchart(squid$nid.length)
+
+#you can play with different things like colours
+dotchart(squid$DML, col="blue")
+
+#or symbols
+dotchart(squid$DML, pch=16)
+
+#combine the two
+dotchart(squid$DML, pch=16, col="blue")
+
+#you can find all the pch values here: http://www.sthda.com/english/wiki/r-plot-pch-symbols-the-different-point-shapes-available-in-r
+
+
+#If you prefer to create a single figure with all 4 plots you can always split your plotting device into 2 rows and 2 columns. 
+par(mfrow=c(2,2))
+dotchart(squid$DML, main="DML")
+dotchart(squid$weight, main="Weight")
+dotchart(squid$nid.length, main="nid.length")
+dotchart(squid$ovary.weight, main="ovary weight")
+
+
+#########HISTOGRAMS########
+
+#When exploring your data it is often useful to visualise the distribution of continuous variables. 
+#create histograms for the variables; DML, weight, eviscerate.weight and ovary.weight using the hist() function
+
+par(mfrow=c(2,2))
+hist(squid$DML, main="DML")
+hist(squid$weight, main="Weight")
+hist(squid$eviscerate.weight, main="Eviscerate.weight")
+hist(squid$ovary.weight, main="Ovary.weight")
+
+
+######SCATTERPLOTS #######
+
+#Scatterplots are great for visualising relationships between two continuous variables. 
+#Plot the relationship between DML on the x axis and weight on the y axis. 
+par(mfrow=c(1,1))
+plot(x=squid$DML, y=squid$weight)
+
+#How would you describe this relationship? Is it linear? 
+
+
+#####BOXPLOTS ######
+#When visualising differences in a continuous variable between levels of a factor (categorical variable) then a boxplot is your friend 
+#Create a boxplot to visualise the differences in DML at each maturity stage (don’t forget to use the recoded version of this variable you created)
+boxplot(DML ~ maturity.stage_f, data = squid, ylab = "DML", xlab = "Maturity stage")
+
+#The thick horizontal line in the middle of the box is the median value of DML. 
+#The upper line of the box is the upper quartile (75th percentile) and the lower line is the lower quartile (25th percentile). 
+#The distance between the upper and lower quartiles is known as the inter quartile range and represents the values of weight for 50% of the data. 
+#The dotted vertical lines are called the whiskers and their length is determined as 1.5 x the inter quartile range. 
+#Data points that are plotted outside the the whiskers represent potential unusual observations. 
+
+
+##### USING GGPLOT2 ########
+#These figures are perfectly fine to include in a paper, but often, using ggplot creates "nicer" figures
+install.packages("ggplot2")
+library("ggplot2")
+
+hist_figure <- ggplot(squid, aes(x=DML)) + #we name our plot so that we can call it at any time
+  #the + here is very important! This is how you add elements to your plot
+  geom_histogram() + #so here, we are saying "make a ggplot object" AND create a histogram from the data
+  labs(title="DML", x="DML", y="Frequency")
+  
+
+plot(hist_figure)
+
+#you can make multiple histograms for DML by maturity stage
+hist_4 <- hist_figure + #and now we can take the data from figure1 AND add the multiple panels
+  facet_wrap(~ maturity.stage_f, nrow = 2) +
+  theme_minimal() #this removes the grey background
+
+plot(hist_4)
+
+
+#create a boxplot
+box_figure<- ggplot(squid, aes(x=maturity.stage_f, y=DML)) +
+  geom_boxplot() +
+  theme_minimal()
+
+plot(box_figure)
+
+##you can do a lot with gggplot, you can find more details for boxplots here: http://www.sthda.com/english/wiki/ggplot2-box-plot-quick-start-guide-r-software-and-data-visualization
+
+##one of the best things about ggplot is that it's easy to save a high resolution figure 
+ggsave("boxplot_DML.jpeg", box_figure, device="jpeg", dpi=1500 )
+#the dpi is "dots per inch" and is your resolution. journals require high res images
+#you can change jpeg to png if you want too!
+
---
+++ Spatial_maps.R
@@ -0,0 +1,200 @@
+##Yogyakarta workshop: creating maps in R
+
+#libraries
+install.packages("terra")
+#install.packages("ggplot2")
+library("terra")
+library("ggplot2")
+
+
+#set your working directory
+setwd() #this is the path to where you have stored the data
+#for example, if you saved your data in Documents/workshop, your path would be "C:\Documents\workshop"
+setwd("C:/Users/Rey/Documents/Indonesia workshop/")
+
+##read in a shape file and plot in terra?
+#the map of Indonesia came from https://gadm.org/download_country.html
+
+indo_0 <- vect("gadm41_IDN_shp/gadm41_IDN_0.shp") 
+plot(indo_0)
+
+#more detail, districts
+indo_1<- vect("gadm41_IDN_shp/gadm41_IDN_1.shp")
+plot(indo_1)
+
+#EVEN more detail
+indo_2<- vect("gadm41_IDN_shp/gadm41_IDN_2.shp")
+plot(indo_2)
+
+#etc
+
+##we will continue on with indo_0
+#to make our lives easier, we turn this into an sf object
+#you don't need to worry about why, it is for conserving latitude/longitude
+indo_sf <- st_as_sf(indo_0)
+
+
+#we are using the package ggplot2 which creates very nice publication-quality figures
+#and is very flexible 
+
+#we can plot all of Indonesia in a basic plot
+figure1 <-  #we name our plot so that we can call it at any time
+  
+  ggplot() + #this is the basic ggplot object. you need to call this anytime you make a new ggplot
+  
+  #the + here is very important! This is how you add elements to your plot
+  #so here, we are saying "make a ggplot object" AND take data from indo_0, with these colours
+  geom_sf(data = indo_sf, # sf format
+          color = "black",
+          fill = "gray90"
+  )
+
+#
+
+plot(figure1)
+
+#and now we add elements that we like
+
+#maybe we don't want the whole country, maybe we want only one island
+#let's say we want Java
+
+figure2 <- figure1 + #and now we can take the data from figure1 AND add coord_sf details
+  
+  coord_sf(xlim = c(100, 130), #x axis coordinates (longitude)
+           ylim = c(-11, -5), #y axis coordinates (latitude)
+           expand = FALSE)
+
+plot(figure2)
+
+
+#you have your map!
+#now we can play with what it looks like
+
+#how about making it print the degree symbols on the axes
+
+figure3 <- figure2 + #take figure2 AND edit the axes
+  
+  scale_x_continuous(labels = ~ paste0(.x, "°", "N")) + #the scale_x_continuous() object will edit the x axis
+  
+  scale_y_continuous(labels = ~ paste0(.x, "°", "E")) #the scale_y_continuous() object will edit the y axis
+
+plot(figure3)
+  
+
+##If we want to get rid of the grey background
+
+figure4<- figure3 +
+  
+  theme(panel.background = element_blank(), #this gets rid of the background
+        axis.line = element_line(), #this makes sure the axis line is still there
+        panel.border = element_rect(colour = "black", fill=NA, linewidth=1)) #add a box around it
+
+plot(figure4)
+
+###there are many MANY things you can edit in ggplot
+#if you are curious, you can go to this site: https://www.rdocumentation.org/packages/ggplot2/versions/3.5.0
+#if you scroll down, there is a long list of functions you can add for specific things
+
+
+#we will play with a couple more
+
+#adding study sites
+
+#create a dataframe of your coordinates you want to plot
+sites<- data.frame(Lat=c(-7, -7.5), Long=c(110,113)) 
+
+figure5 <- figure4 +
+  geom_point(data = sites,
+             aes(x = Long,
+                 y = Lat),
+             size = 5, #how big are the dots?
+             shape = 21, #21 is the code for dots
+             fill = "black" #colour
+  )
+plot(figure5)
+
+#you can play with the size, shape and colour!
+
+
+#let's add some text to your map
+
+figure6 <- figure5 +
+  annotate("text",
+         label = c("JAVA"),
+         x = c(105),
+         y = c(-9),
+         color = "gray48",
+         size = 5.5)
+plot(figure6)
+
+
+
+##ok we've seen some of the basics, let's save our plot in high resolution
+ggsave("Java.jpeg", figure6, device="jpeg", dpi=1500 )
+#the dpi is "dots per inch" and is your resolution. journals require high res images
+#you can change jpeg to png if you want too!
+
+
+##if you want to get really fancy, you can create this Java map with an inset of the larger Indonesia map
+
+
+#first we create the insert
+inset_map <- indo_sf %>%
+  ggplot() + #it gets its own ggplot object
+  
+  geom_sf() + 
+  
+  geom_rect(aes(xmin = 100, #we create a rectangle within Indonesia that shows where Java is
+                xmax = 130,
+                ymin =-11,
+                ymax = -5),
+            color = "red",
+            linewidth = 1,
+            fill = NA) +
+  
+  labs(x = NULL,
+       y = NULL) +
+  
+  #and we play with all the theme elements, getting rid of axes etc 
+  theme(axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.ticks.length = unit(0, "pt"),
+        axis.title=element_blank(),
+        plot.margin = margin(0, 0, 0, 0, "cm"),
+        panel.grid.major = element_blank(),
+        panel.background = element_rect(fill = "lightskyblue"),
+        panel.border = element_rect(fill = NA,
+                                    linewidth = 1),
+        
+  ) +
+  
+  scale_x_continuous(expand=c(0,0)) +
+  scale_y_continuous(expand=c(0,0)) +
+  
+  #and add a label
+  annotate("text",
+           label = "INDONESIA",
+           x = c(125),
+           y = c(4),
+           color = "black",
+           size = c(3),
+           fontface = "bold"
+  )
+  
+  
+#then we simply add it to our figure6 in the correct place
+
+figure7 <- figure6 +
+  inset_element(inset_map,
+                left = 0.7,
+                bottom = 0.7,
+                right = 1,
+                top = 1,
+                align_to = "full"
+  )
+
+  plot(figure7)
+
+#and save!
+ggsave("Java_insert.jpeg", figure7, device="jpeg", dpi=1500 )
+#remember to change it to figure7!! and to change the name of the file, otherwise it will overwrite the previous one
